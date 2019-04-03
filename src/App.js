c5ef815ec7d63648f70bcef00805e8bca390cb07
Test <justnisar@gmail.com>
2019-04-03 00:20:12 -0700

    tuesday commit

--- src/App.js
+++ src/App.js
@@ -29,19 +29,12 @@ class App extends Component {
     else{
       output = "Length too short";
     }
-    let print = [...this.state.text];
-    console.log(print);
-    let printthis = (
-         <div>
-           
-           {[...this.state.text].forEach(
-              (c) =>  {return <CharComponent ch={c}/>}
-            )}
-          
-        </div>
 
+    let printthis = (    
+            [...this.state.text].map(c => 
+              <CharComponent ch={c}/>
+            )
     );
-  
 
     return (
       <div className="App">
@@ -50,7 +43,7 @@ class App extends Component {
           <li> <strike>Create a new component (=> ValidationComponent) which receives the text length as a prop</strike></li>
           <li> <strike>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length) </strike></li>
           <li> <strike>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black). </strike></li>
-          <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
+          <li> <strike>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop. </strike></li>
           <li>When you click a CharComponent, it should be removed from the entered text.</li>
         </ol>
         <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
@@ -59,9 +52,7 @@ class App extends Component {
 
         <p> The length of the text entered : {this.state.textLength}</p>
         <ValidationComponent output={output}/>
-        {printthis}
-        <CharComponent ch='shaik'/>
-        
+        {printthis}    
       </div>
     );
   }
--- src/CharComponent.js
+++ src/CharComponent.js
@@ -2,9 +2,9 @@
 import React from 'react';
 const CharComponent = (props) => {
     return (
-            <div>
-            <p>{props.ch}</p>
-            </div>
+
+        <p>{props.ch}</p>
+
     );
 }
 
